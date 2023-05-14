0fb8211904a4a6af7c983ea8b5ed243959055931
Kashalls <kashalls@pm.me>
2023-05-14 14:54:22 -0700

    Add `mineflayer-auto-eat`

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import * as mineflayer from 'mineflayer'
+import * as autoEat from 'mineflayer-auto-eat'
+
+const processKey = 'VSCODE_'
+
+const processEntries = Object.entries(process.env);
+const filteredEntries = processEntries.filter(([key]) => key.startsWith(processKey));
+const mappedEntries = filteredEntries.map(([key, value]) => [key.replace(processKey, '').toLowerCase(), value]);
+const mineflayerOptions = Object.fromEntries(mappedEntries)
+
+const bot = mineflayer.createBot(mineflayerOptions)
+bot.loadPlugin(autoEat.plugin)
+
+bot.on('message', (message) => {
+  console.log(message.toAnsi())
+})
